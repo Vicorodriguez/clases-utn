ac1653fb87764500724ae3332469ae4e114c5318
Vicorodriguez <rodriguez.va02@hotmail.com>
2025-10-22 18:27:41 -0300

    agrego  nuevas carpetas

---
+++ 10/index.js
@@ -0,0 +1,59 @@
+// Data types
+
+// Nativos
+
+// String - Cadena de caracteres
+"Hola"; 'Hola'; `Hola`; "20";
+
+// Numbers
+20; 0.5; 1.120;
+
+// Boolean
+true; false;
+
+// Otros
+// Null significa nulo, es configurado por desarrollador
+// Null significa que un campo, o dato existente no tiene valor
+null;
+
+// No esta definido o no existe
+undefined;
+
+// Data types - Javascript
+
+// Objeto / Object
+// Pares {clave: valor}
+// Usamos objeto cuando queremos que una entidad tenga propiedades/ caracteristicas/ atributos
+
+// Entidad persona
+{ nombre: "javier", apellido: "lopez", mascotas: null, argentino: true }
+
+{ nombre: "juancito", apellido: "gutierrez", mascotas: ["Toby"], argentino: false }
+
+// Array / Arreglo / Lista
+// El array es un listado de elementos
+// Se "supone" que los elementos de un array son todos del mismo tipo
+// Usamos arrays cuando queremos tener un listado de elementos que sean del mismo tipo
+[1, 2, 3, 4, 5, 6, 7, 8, { nombre: "javier" }, true, [0, 2]]
+
+//Listado de personas
+[
+{ 
+    nombre: "javier",
+    apellido: "lopez",
+    mascotas: null,
+    argentino: true
+},
+{
+    nombre: "juancito",
+    apellido: "gutierrez",
+    mascotas: ["Toby"],
+    argentino: false
+     },
+     {
+    nombre: "Mark",
+    apellido: "gutierrez",
+    mascotas: null,
+    argentino: true
+     }
+]
